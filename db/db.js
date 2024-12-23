70d646fd31056ac88e37c2a2f19cd956f1dd6fc4
unknown <farrelfatih22@gmail.com>
2024-12-23 08:30:29 +0700

    update pool pg

--- db/db.js
+++ db/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "janken_db",
-  password: "123456",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
