e550ef079d0d10149a4a19e9b365168d75b79df9
parhan <parhansuryana1@gmail.com>
2024-12-24 20:28:14 +0700

    change login schema with username

--- controllers/users.js
+++ controllers/users.js
@@ -10,7 +10,7 @@ const registerSchema = Joi.object({
 });
 
 const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
+  username: Joi.string().required(),
   password: Joi.string().required(),
 });
 
